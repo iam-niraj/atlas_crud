bebe6026391344227b7902b0b782e72ff47dfbfd
NirajMestry <76642650+iam-niraj@users.noreply.github.com>
2023-02-11 09:50:01 +0530

    atlas_crud

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+const MONGO_DB_CONFIG = {
+  DB: "mongodb+srv://admin:123@ecsattendance.xwoaovn.mongodb.net/?retryWrites=true&w=majority"  
+};
+
+//"mongodb://localhost/ecommerce-app"
+
+module.exports = {
+  MONGO_DB_CONFIG,
+};
